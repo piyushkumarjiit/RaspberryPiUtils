b5f53854fe8647de56aca8cef05eddf8a7650108
Piyush Kumar <piyushkumar.jiit@gmail.com>
2020-01-08 19:09:57 -0600

    Added logging

--- HardCodedDNSFilter.sh
+++ HardCodedDNSFilter.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+exec &>> /home/pi/piCron-log.txt
+echo "Running cron-job HardCodedDNSFilter on $(date)"
 #Get IP Address of your PiHole
 PiHoleIP=$(hostname -I)
 #PiHoleIP='<yourPiHoleIP>'
@@ -14,5 +16,5 @@ RouterPort='8022'
 #Connect to your DD-WRT router via SSH. Assuming you have set up key based SSH login
 #You can execute/follow other script to get this to work
 echo "Calling shell script to update Pre Routing on Router with Router IP:$RouterIP and Port:$RouterPort for PiHole IP:$PiHoleIP"
-ssh admin@$RouterIP -p$RouterPort 'sh -s' < PreRoutingForPi.sh $PiHoleIP
+ssh admin@$RouterIP -p$RouterPort 'sh -s' < /home/pi/PreRoutingForPi.sh $PiHoleIP
 echo "Processing complete"
